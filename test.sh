ed6edd4586a88deccec6ef4343d6809cfee5edae
GY <35892608+gygyggy@users.noreply.github.com>
2021-03-29 16:37:34 +0800

    update

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
 demoFun() {
-  array=( A B C D 1 2 3 4)
+  array=(A B C D 1 2 3 4)
   for element in ${array[*]}; do
     echo "$element"
   done
 }
 
+jq .children "./structure.json"| while read i;
+     do stuff with $i
+done
+#echo json
 demoFun
